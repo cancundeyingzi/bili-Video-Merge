f6d4e0aad3aa07abf43af8632a168d0264d3196d
影子 <73635883+cancundeyingzi@users.noreply.github.com>
2022-12-09 10:12:48 +0800

    Add files via upload

--- bili合并.py
+++ bili合并.py
@@ -1,7 +1,21 @@
 import os
 import time
 start = time.perf_counter()
+
+
+检测文件夹缓存=os.listdir()
+文件夹存在=False
+for 检测文件夹缓存1 in 检测文件夹缓存:
+    if 检测文件夹缓存1=="bili合并":
+        文件夹存在=True
+
+if 文件夹存在==False:
+    os.mkdir('./bili合并')
+    print("创建文件夹bili合并")
+
+
 当前目录文件=os.listdir('./bili合并')
+
 print(当前目录文件)
 筛选后文件=[]
 for 文件名 in 当前目录文件:
@@ -27,13 +41,6 @@ for i in range(0,len(筛选后文件) - 1,2):
     文件名p = 筛选后文件[i + 1]
     a='ffmpeg -y -hide_banner -i "./bili合并/'+文件名+'" -i "./bili合并/'+文件名p+'" -c:v copy -b:a 256k "./bili合并/_'+文件名p+'"'
     os.system(a)
-
-
-
-
-
-
-
 
 
 end = time.perf_counter()
